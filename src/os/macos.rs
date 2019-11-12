c522870aa6d68a2e67d128280c40f5d1f860196e
etoledom <etoledom@icloud.com>
2019-11-12 16:08:42 +0100

    [MacOS] Moving repeated code into common file

--- src/os/macos.rs
+++ src/os/macos.rs
@@ -1,34 +1,15 @@
 use ::pnet::datalink::Channel::Ethernet;
 use ::pnet::datalink::DataLinkReceiver;
 use ::pnet::datalink::{self, Config, NetworkInterface};
-use ::std::io::{self, stdin, Write};
-use ::termion::event::Event;
-use ::termion::input::TermRead;
-
 use ::std::collections::HashMap;
-use ::std::net::IpAddr;
-
-use signal_hook::iterator::Signals;
 
 use crate::network::{Connection};
-use crate::OsInputOutput;
 
 use std::net::{SocketAddr};
 use super::lsof_utils;
+use crate::os::lsof_utils::RawConnection;
 
-struct KeyboardEvents;
-
-impl Iterator for KeyboardEvents {
-    type Item = Event;
-    fn next(&mut self) -> Option<Event> {
-        match stdin().events().next() {
-            Some(Ok(ev)) => Some(ev),
-            _ => None,
-        }
-    }
-}
-
-fn get_datalink_channel(
+pub fn get_datalink_channel(
     interface: &NetworkInterface,
 ) -> Result<Box<dyn DataLinkReceiver>, failure::Error> {
     match datalink::channel(interface, Config::default()) {
@@ -38,13 +19,7 @@ fn get_datalink_channel(
     }
 }
 
-fn get_interface(interface_name: &str) -> Option<NetworkInterface> {
-    datalink::interfaces()
-        .into_iter()
-        .find(|iface| iface.name == interface_name)
-}
-
-fn get_open_sockets() -> HashMap<Connection, String> {
+pub fn get_open_sockets() -> HashMap<Connection, String> {
     let mut open_sockets = HashMap::new();
 
     let connections = lsof_utils::get_raw_connections_output();
@@ -68,61 +43,4 @@ fn get_open_sockets() -> HashMap<Connection, String> {
     }
 
     return open_sockets;
-}
-
-fn lookup_addr(ip: &IpAddr) -> Option<String> {
-    ::dns_lookup::lookup_addr(ip).ok()
-}
-
-fn sigwinch() -> (Box<dyn Fn(Box<dyn Fn()>) + Send>, Box<dyn Fn() + Send>) {
-    let signals = Signals::new(&[signal_hook::SIGWINCH]).unwrap();
-    let on_winch = {
-        let signals = signals.clone();
-        move |cb: Box<dyn Fn()>| {
-            for signal in signals.forever() {
-                match signal {
-                    signal_hook::SIGWINCH => cb(),
-                    _ => unreachable!(),
-                }
-            }
-        }
-    };
-    let cleanup = move || {
-        signals.close();
-    };
-    (Box::new(on_winch), Box::new(cleanup))
-}
-
-pub fn create_write_to_stdout() -> Box<dyn FnMut(String) + Send> {
-    Box::new({
-        let mut stdout = io::stdout();
-        move |output: String| {
-            writeln!(stdout, "{}", output).unwrap();
-        }
-    })
-}
-
-pub fn get_input(interface_name: &str) -> Result<OsInputOutput, failure::Error> {
-    let keyboard_events = Box::new(KeyboardEvents);
-    let network_interface = match get_interface(interface_name) {
-        Some(interface) => interface,
-        None => {
-            failure::bail!("Cannot find interface {}", interface_name);
-        }
-    };
-    let network_frames = get_datalink_channel(&network_interface)?;
-    let lookup_addr = Box::new(lookup_addr);
-    let write_to_stdout = create_write_to_stdout();
-    let (on_winch, cleanup) = sigwinch();
-
-    Ok(OsInputOutput {
-        network_interface,
-        network_frames,
-        get_open_sockets,
-        keyboard_events,
-        lookup_addr,
-        on_winch,
-        cleanup,
-        write_to_stdout,
-    })
 }
--- src/os/mod.rs
+++ src/os/mod.rs
@@ -1,13 +1,10 @@
 #[cfg(target_os = "linux")]
 mod linux;
 
-#[cfg(target_os = "linux")]
-pub use linux::*;
-
 #[cfg(target_os = "macos")]
 mod macos;
 
-#[cfg(target_os = "macos")]
-pub use macos::*;
-
 mod lsof_utils;
+mod common;
+
+pub use common::*;
